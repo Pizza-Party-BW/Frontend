610af2e13129554354732bf510eb8dba430e32d9
May Ng <mngmay@gmail.com>
2020-01-06 12:21:37 -0800

    can login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import axios from "axios";
 
-const LoginForm = () => {
-  return <div></div>;
+const LoginForm = props => {
+  const [user, setUser] = useState({ username: "", password: "" });
+
+  console.log(user);
+  useEffect(() => {});
+
+  function handleChange(e) {
+    const updatedUser = { ...user, [e.target.name]: e.target.value };
+    setUser(updatedUser);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .post("https://lambda-mud-test.herokuapp.com/api/login/", user)
+      .then(res => {
+        console.log("Logged in!", res.data);
+      })
+      .catch(err => {
+        console.log("Error occured!: ", err.response);
+      });
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          required
+          placeholder="Username"
+          value={user.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          required
+          placeholder="Password"
+          value={user.password}
+          onChange={handleChange}
+        />
+        <button>Login</button>
+      </form>
+    </>
+  );
 };
 
-export default LoginForm;
+export default withRouter(LoginForm);
