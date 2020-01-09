07e13074a57ddba9f97d8738ea1a27c1ea753155
May Ng <mngmay@gmail.com>
2020-01-09 15:15:05 -0800

    refactored player details into player component

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -1,8 +1,29 @@
 import React from "react";
-import { turtle } from "./assets";
+import { useStateValue } from "../../hooks/useStateValue";
+import turtleR from "../player/assets/turtle-r.png";
+import turtleL from "../player/assets/turtle-l.png";
 
 const Player = props => {
-  return <div></div>;
+  const [{ gameState }, dispatch] = useStateValue();
+
+  const x = gameState.location.x;
+  const y = gameState.location.y;
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${turtleR})`,
+        width: 36,
+        height: 36,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        margin: "auto auto",
+        gridColumn: `${x + 1} / ${x + 2}`,
+        gridRow: `${(y + 1) * -1} / ${(y + 2) * -1}`,
+        zIndex: 110
+      }}
+    ></div>
+  );
 };
 
 export default Player;
